0f91ff9b5e45bbaffcb6e750662d993f54ca844e
minji <104603852+mjkim05@users.noreply.github.com>
2024-01-24 17:51:13 +0900

    Add files via upload

---
+++ SpringMvcStudy/MybatisWork02/src/com/test/mybatis/IMemberDAO.java
@@ -0,0 +1,20 @@
+/* ==================
+ 	IMemberDAO.java
+ 	- 인터페이스
+ ====================*/
+
+package com.test.mybatis;
+
+import java.util.ArrayList;
+
+public interface IMemberDAO
+{
+	public ArrayList<MemberDTO> list();
+	public int count();
+	public int add(MemberDTO m);
+	
+	/* 삭제 기능 처리 ②*/
+	
+	public int delete(String mid);
+	
+}
